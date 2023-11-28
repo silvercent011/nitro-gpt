1db3cb9d2100330def52690b76df489f7e372023
Sidney Alex <silvercenturion2@gmail.com>
2023-11-28 00:25:45 -0300

    feat(client): adiciona cliente nuxt

---
+++ client/nuxt.config.ts
@@ -0,0 +1,6 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ["@nuxt/devtools"],
+  devtools: { enabled: true },
+  serverDir: "../api",
+});
